40132ca4b58050f7dba230f8ab88e5b29ff60f9f
EugeneHuman <ydjents@gmail.com>
2022-10-23 18:17:57 +0300

    12. Separeiting by components

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDd4OzuInxGVL6HTTo-es0TbsktfIwVhZksQ&usqp=CAU" />
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className="content">
+      Profile
+      <div>
+        <img src="https://www.netscribes.com/wp-content/uploads/2020/12/telemedicine_technology_blog_image.jpg" />
+      </div>
+      <div>ava + description</div>
+      <div>
+        my post
+        <div>new post</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
