f30c99550313701ee574d6abcadb046652e02a57
EugeneHuman <ydjents@gmail.com>
2022-10-25 19:30:26 +0300

    19. Route, browser-router, маршрутизация

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
 import './App.css'
 
 import Profile from './components/Profile/Profile'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Dialogs from './components/Dialogs/Dialogs'
+import Settings from './components/Settings/Settings'
+import Music from './components/Music/Music'
+import News from './components/News/News'
 
 function App() {
   return (
@@ -12,8 +17,13 @@ function App() {
       <Header />
       <Navbar />
       <div className="app-wrapper-content">
-        <Dialogs />
-        {/* <Profile /> */}
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/dialogs" element={<Dialogs />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
       </div>
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
+
 import s from './Navbar.module.css'
 
 const Navbar = () => {
   return (
     <div className={s.navbar}>
-
-        <div className={`${s.item} ${s.active}`}>
-          <a href="#">Profile</a>
-        </div>
-        <div className={s.item}>
-          <a href="#">Messages</a>
-        </div>
-        <div className={s.item}>
-          <a href="#">News</a>
-        </div>
-        <div className={s.item}>
-          <a href="#">Music</a>
-        </div>
-        <div className={s.item}>
-          <a href="#">Settings</a>
-        </div>
+      <div className={`${s.item} ${s.active}`}>
+        <a href="/profile">Profile</a>
+      </div>
+      <div className={s.item}>
+        <a href="/dialogs">Messages</a>
+      </div>
+      <div className={s.item}>
+        <a href="/news">News</a>
+      </div>
+      <div className={s.item}>
+        <a href="/music">Music</a>
+      </div>
+      <div className={s.item}>
+        <a href="/settings">Settings</a>
+      </div>
     </div>
   )
 }
